7e25cda99468849e1db29119b8be32138c33c704
ninavaneijk <94168671+ninavaneijk@users.noreply.github.com>
2021-12-14 17:16:17 +0100

    Add files via upload

--- Nina-Eijkvan_PARADISO_02-12-2021/scripts/script.js
+++ Nina-Eijkvan_PARADISO_02-12-2021/scripts/script.js
@@ -4,9 +4,50 @@
 
 var menuButton = document.querySelector("button");
 
-function naarBenee(){
+menuButton.addEventListener('click', naarBenee);
+
+function naarBenee() {
     let menu = document.querySelector("div:first-of-type");
     menu.classList.toggle("hamburgerOpen");
 }
 
-menuButton.addEventListener("click", naarBenee); 
+
+
+// hamburger menu van burger naar kruis laten gaan //
+
+menuButton.addEventListener("click", vanBurgerNaarKruis);
+
+function vanBurgerNaarKruis() {
+    var deNav = document.querySelector("nav:first-of-type");
+    deNav.classList.toggle("kruis");
+}
+
+//Dark modus inschakelen// 
+
+var darkBox = document.querySelector(".indieHeader>nav>section>input");
+darkBox.addEventListener('change', hetWordtDonker);
+
+
+function hetWordtDonker() {
+    if (darkBox.checked) {
+        document.documentElement.classList.add("darkmode");
+    }
+    else {
+        document.documentElement.classList.remove("darkmode");
+    }
+}
+
+// kerst thema! // 
+
+var kerstBox = document.querySelector("header>nav:first-of-type>section>input");
+
+kerstBox.addEventListener('change', kerstThemaAan);
+
+function kerstThemaAan() {
+    if (kerstBox.checked) {
+        document.documentElement.classList.add("kerstThema");
+    }
+    else {
+        document.documentElement.classList.remove("kerstThema");
+    }
+}
